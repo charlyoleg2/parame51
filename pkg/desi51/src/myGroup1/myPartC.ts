a9311dcf71b3dd4299e2b391d6310c70a5a0a609
charles <charles.braquet@e.email>
2024-01-15 22:42:36 +0100

    defining myPartC

--- pkg/desi51/src/myGroup1/myPartC.ts
+++ pkg/desi51/src/myGroup1/myPartC.ts
@@ -32,15 +32,15 @@ const pDef: tParamDef = {
 	partName: 'myPartC',
 	params: [
 		//pNumber(name, unit, init, min, max, step)
-		pNumber('D1', 'mm', 40, 10, 100, 2),
-		pNumber('D2', 'mm', 40, 10, 100, 2),
-		pNumber('D3', 'mm', 40, 10, 100, 2),
-		pNumber('D4', 'mm', 40, 10, 100, 2),
-		pNumber('L1', 'mm', 3, 1, 50, 1),
-		pNumber('L2', 'mm', 3, 1, 50, 1),
-		pNumber('L3', 'mm', 3, 1, 50, 1),
-		pNumber('L4', 'mm', 3, 1, 50, 1),
-		pNumber('H1', 'mm', 50, 10, 200, 10)
+		pNumber('D1', 'mm', 40, 10, 200, 1),
+		pNumber('D2', 'mm', 100, 20, 200, 1),
+		pNumber('D3', 'mm', 30, 10, 100, 1),
+		pNumber('D4', 'mm', 50, 20, 100, 1),
+		pNumber('L1', 'mm', 100, 10, 300, 1),
+		pNumber('L2', 'mm', 100, 10, 300, 1),
+		pNumber('L3', 'mm', 20, 10, 300, 1),
+		pNumber('L4', 'mm', 40, 1, 300, 1),
+		pNumber('H1', 'mm', 50, 10, 100, 1)
 	],
 	paramSvg: {
 		D1: 'myPartC_side.svg',
@@ -63,77 +63,130 @@ const pDef: tParamDef = {
 // step-3 : definition of the function that creates from the parameter-values the figures and construct the 3D
 function pGeom(t: number, param: tParamVal): tGeom {
 	const rGeome = initGeom(pDef.partName);
-	const figSection = figure();
-	const figSide = figure();
+	const figBodyCut = figure();
+	const figBodySlant = figure();
+	const figChimney = figure();
+	const figChimneyHollow = figure();
 	rGeome.logstr += `${rGeome.partName} simTime: ${t}\n`;
 	try {
 		// step-4 : some preparation calculation
 		const R1 = param.D1 / 2;
+		const R2 = param.D2 / 2;
+		const R3 = param.D3 / 2;
+		const R4 = param.D4 / 2;
 		// step-5 : checks on the parameter values
-		if (R1 < param.E1) {
-			throw `err089: D1 ${param.D1} too small compare to E1 ${param.E1}`;
+		if (param.D1 > param.D2) {
+			throw `err077: D1 ${param.D1} too large compare to D2 ${param.D2}`;
+		}
+		if (param.D4 > param.D2) {
+			throw `err080: D4 ${param.D4} too large compare to D2 ${param.D2}`;
+		}
+		if (param.D3 > param.D4) {
+			throw `err083: D3 ${param.D3} too large compare to D4 ${param.D4}`;
+		}
+		if (param.L1 < param.D4 + param.L4) {
+			throw `err086: L1 ${param.L1} too small compare to D4 ${param.D4} and L4 ${param.L4}`;
+		}
+		if (param.L3 > param.L1 + param.L2) {
+			throw `err089: L3 ${param.L3} too large compare to L1 ${param.L1} and L2 ${param.L2}`;
 		}
 		// step-6 : any logs
-		rGeome.logstr += `myPartC-length: ${ffix(param.L1)} mm\n`;
-		rGeome.logstr += `myPartC-external-diameter: ${ffix(param.D1)} mm\n`;
-		rGeome.logstr += `myPartC-internal-diameter: ${ffix(param.D1 - 2 * param.E1)} mm\n`;
+		rGeome.logstr += `myPartC-length: ${ffix(param.L1 + param.L2)} mm\n`;
+		rGeome.logstr += `myPartC-height: ${ffix(param.D2 + param.H1)} mm\n`;
 		// step-7 : drawing of the figures
-		// figSection
-		figSection.addMain(contourCircle(0, 0, R1));
-		figSection.addMain(contourCircle(0, 0, R1 - param.E1));
-		// figSide
-		const ctrCylinderSideRight = contour(R1, 0)
-			.addSegStrokeA(R1, param.L1)
-			.addSegStrokeA(R1 - param.E1, param.L1)
-			.addSegStrokeA(R1 - param.E1, 0)
+		// figBodyCut
+		const ctrBodyCut1 = contour(-R2, 0)
+			.addSegStrokeA(-R2, param.L1)
+			.addSegStrokeA(-R1, param.L1 + param.L2)
+			.addSegStrokeR(0, -param.L3)
 			.closeSegStroke();
-		const ctrCylinderSideLeft = contour(-R1, 0)
-			.addSegStrokeR(param.E1, 0)
-			.addSegStrokeR(0, param.L1)
-			.addSegStrokeR(-param.E1, 0)
+		const ctrBodyCut2 = contour(R2, 0)
+			.addSegStrokeA(R2, param.L1)
+			.addSegStrokeA(R1, param.L1 + param.L2)
+			.addSegStrokeR(0, -param.L3)
 			.closeSegStroke();
-		figSide.addMain(ctrCylinderSideRight);
-		figSide.addSecond(ctrCylinderSideLeft);
+		figBodyCut.addMain(ctrBodyCut1);
+		figBodyCut.addSecond(ctrBodyCut2);
+		// figBodySlant
+		const ctrBodySlant = contour(-R2, 0)
+			.addSegStrokeA(-R2, param.L1 + param.L2)
+			.addSegStrokeA(R2, param.L1 + param.L2)
+			.addSegStrokeA(R2, param.L4)
+			.closeSegStroke();
+		figBodySlant.addMain(ctrBodySlant);
+		// figChimney
+		figChimney.addMain(contourCircle(0, param.L1 - R4, R4));
+		figChimney.addSecond(contourCircle(0, param.L1 - R4, R3));
+		figChimney.addSecond(ctrBodyCut1);
+		figChimney.addSecond(ctrBodyCut2);
+		// figChimneyHollow
+		figChimneyHollow.addMain(contourCircle(0, param.L1 - R4, R3));
+		figChimneyHollow.addSecond(contourCircle(0, param.L1 - R4, R4));
+		figChimneyHollow.addSecond(ctrBodyCut1);
+		figChimneyHollow.addSecond(ctrBodyCut2);
 		// final figure list
 		rGeome.fig = {
-			faceSection: figSection,
-			faceSide: figSide
+			faceBodyCut: figBodyCut,
+			faceBodySlant: figBodySlant,
+			faceChimney: figChimney,
+			faceChimneyHollow: figChimneyHollow
 		};
 		// step-8 : recipes of the 3D construction
 		const designName = rGeome.partName;
 		rGeome.vol = {
 			extrudes: [
 				{
-					outName: `subpax_${designName}_using_section`,
-					face: `${designName}_faceSection`,
-					extrudeMethod: EExtrude.eLinearOrtho,
-					length: param.L1,
+					outName: `subpax_${designName}_bodyRaw`,
+					face: `${designName}_faceBodyCut`,
+					extrudeMethod: EExtrude.eRotate, // always along the axis 0y
 					rotate: [0, 0, 0],
 					translate: [0, 0, 0]
 				},
 				{
-					outName: `subpax_${designName}_using_side`,
-					face: `${designName}_faceSide`,
-					extrudeMethod: EExtrude.eRotate,
+					outName: `subpax_${designName}_bodySlant`,
+					face: `${designName}_faceBodySlant`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: 2 * R2,
 					rotate: [0, 0, 0],
-					translate: [0, 0, 0]
+					translate: [0, 0, -R2]
+				},
+				{
+					outName: `subpax_${designName}_chimney`,
+					face: `${designName}_faceChimney`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: param.H1 + R2,
+					rotate: [0, 0, 0],
+					translate: [0, 0, -param.H1 - R2]
+				},
+				{
+					outName: `subpax_${designName}_chimneyH`,
+					face: `${designName}_faceChimneyHollow`,
+					extrudeMethod: EExtrude.eLinearOrtho,
+					length: param.H1 + R2,
+					rotate: [0, 0, 0],
+					translate: [0, 0, -param.H1 - R2]
 				}
 			],
 			volumes: [
 				{
-					outName: `ipax_${designName}_intermediate_1`,
-					boolMethod: EBVolume.eUnion, // eIdentity, eIntersection, eUnion, eSubstraction
-					inList: [`subpax_${designName}_using_section`, `ipax_${designName}_using_side`]
+					outName: `ipax_${designName}_body1`,
+					boolMethod: EBVolume.eIntersection, // eIdentity, eIntersection, eUnion, eSubstraction
+					inList: [`subpax_${designName}_bodyRaw`, `subpax_${designName}_bodySlant`]
 				},
 				{
-					outName: `ipax_${designName}_intermediate_2`,
-					boolMethod: EBVolume.eIntersection,
-					inList: [`subpax_${designName}_using_section`, `ipax_${designName}_using_side`]
+					outName: `ipax_${designName}_body2`,
+					boolMethod: EBVolume.eUnion,
+					inList: [`ipax_${designName}_body1`, `subpax_${designName}_chimney`]
+				},
+				{
+					outName: `ipax_${designName}_body3`,
+					boolMethod: EBVolume.eSubstraction,
+					inList: [`ipax_${designName}_body2`, `subpax_${designName}_chimneyH`]
 				},
 				{
 					outName: `pax_${designName}`,
-					boolMethod: EBVolume.eIdentity,
-					inList: [`subpax_${designName}_using_section`]
+					boolMethod: EBVolume.eIdentity, // useless identity but just for showcasing
+					inList: [`ipax_${designName}_body3`]
 				}
 			]
 		};
